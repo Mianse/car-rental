ce13a4119766843281360ad74d2fba60d9fe1a5f
damian orina <96182161+Mianse@users.noreply.github.com>
2024-05-08 06:23:43 +0300

    Update CarList.tsx

--- app/components/Home/CarList.tsx
+++ app/components/Home/CarList.tsx
@@ -7,7 +7,7 @@ const CarList = (props:any) => {
   const [selectedCar,setSelectedCar] = useState<any>([])
 
   return (
-    <div  className=' lg:gap-16  mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-1'>
+    <div  className=' lg:gap-16  mx-auto max-w-screen-xl px-4 py-8 sm:px-10 sm:py-20 lg:px-8 lg:py-16 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-1'>
       {props.carList.map((car: any, index: number)=>(
 <div key={index} onClick={()=>{(window as any).my_modal_4.showModal();
   setSelectedCar(car)
